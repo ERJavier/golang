49ef0c99a77034d6a0a1846580c88725104a614b
Javier Cepeda <erjavierc@gmail.com>
2022-11-14 20:25:32 -0500

    Variables, Constants, Zero Value, calc

--- src/main.go
+++ src/main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// Constant
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi:", pi, " p2:", pi2)
+	fmt.Println()
+
+	// Variables 
+	base := 12
+	var height int = 14
+	var area int
+
+	fmt.Print(base, height, area)
+	fmt.Println()
+
+	// Zero Value
+
+	var a int // 0
+	var b float64 //0
+	var c string  // empty 
+	var d bool // true | false
+
+	fmt.Println(a, b, c, d)
+
+
+	// area cube
+
+	const baseCube = 10
+	areaCube := baseCube * baseCube
+	fmt.Println("cube is: ", areaCube)
 }
